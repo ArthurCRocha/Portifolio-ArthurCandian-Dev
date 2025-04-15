c214147c4d197cdf070bee1fa9ecd8744a54aba4
Arthur Candian Rocha <75858075+ArthurCRocha@users.noreply.github.com>
2025-04-15 13:04:31 -0300

    first commit

---
+++ src/app/home_component/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { AboutComponent } from '../components/about/about.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { ContactComponent } from '../components/contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: 'about' }
+];
